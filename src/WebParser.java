1bdb34ada8f6d95908b907828ec04adda0407e37
M <738819@cognizant.com>
2020-11-22 17:10:01 +0530

    frequency of the words

--- src/WebParser.java
+++ src/WebParser.java
@@ -1,15 +1,13 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 import org.jsoup.Jsoup;
@@ -31,19 +29,39 @@ public class WebParser {
 	 */
 	public static void main(String[] args) {
 		try {
+
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			String url = br.readLine();
 			HashSet<String> wordList = new HashSet<>();
 			parsePage(url, new LinkedList<>(), new URL(url).getHost(), wordList);
-			/*
-			 * Map<Object, Integer> map = wordList.stream().map(val ->
-			 * val.split(" ")).flatMap(Arrays::stream)
-			 * .collect(Collectors.toList()).stream() .collect(Collectors.toMap(val ->
-			 * val.toString(), val -> 1, Integer::sum)); map.forEach((k, v) -> {
-			 * System.out.println("Key :" + k + " Value : " + v); });
-			 */
+			// Word pair frequency
+			LinkedList<String> al = new LinkedList<>();
+			AtomicReference<String> prevWord = new AtomicReference<>();
+			wordList.forEach(val -> {
+				String[] strArr = val.split(" ");
+				if (strArr.length > 0) {
+					prevWord.set(strArr[0]);
+					if (strArr.length == 1)
+						al.add(strArr[0]);
+					for (int i = 1; i < strArr.length; i++) {
+						al.add(prevWord + " " + strArr[i]);
+						prevWord.set(strArr[i]);
+					}
+				}
+			});
+			al.stream().collect(Collectors.toMap(val -> val.toString(), val -> 1, Integer::sum)).entrySet().stream()
+					.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).limit(10).forEach(action -> {
+						System.out.println("Word : " + action.getKey() + " Frequency : " + action.getValue());
+					});
+			// Word Frequency
+			System.out.println();
+			wordList.stream().map(val -> val.split(" ")).flatMap(Arrays::stream).collect(Collectors.toList()).stream()
+					.collect(Collectors.toMap(val -> val.toString(), val -> 1, Integer::sum)).entrySet().stream()
+					.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).limit(10).forEach(action -> {
+						System.out.println("Word : " + action.getKey() + " Frequency : " + action.getValue());
+					});
 		} catch (Exception e) {
-			System.out.print("Error while parsing " + e);
+			System.out.println("Error while parsing " + e);
 		}
 	}
 
@@ -51,7 +69,7 @@ public class WebParser {
 			HashSet<String> outputList) {
 		try {
 			if (isURLValid(url, hostName) && !processedURL.contains(url)) {
-				//System.out.println(url);
+				// System.out.println(url);
 				Document htmlDom = Jsoup.connect(url).get();
 				processedURL.add(url);
 				List<Node> nodes = htmlDom.body().childNodes();
@@ -76,7 +94,7 @@ public class WebParser {
 
 				} else {
 					if (value.nodeName().startsWith("#text")) {
-						outputList.add(value.toString());
+						outputList.add(value.toString().trim());
 					}
 				}
 			});
